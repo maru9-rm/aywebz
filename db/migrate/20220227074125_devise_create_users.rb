7ab54f713360cbee391662fb2d78bddf81d1c770
Ryota9 <ngm9dwf@gmail.com>
2022-03-01 14:15:48 +0900

    adminカラムの追加とcancancanの導入。

--- db/migrate/20220227074125_devise_create_users.rb
+++ db/migrate/20220227074125_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :encrypted_password, null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
