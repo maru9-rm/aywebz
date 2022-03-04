78f0ece7b28eba4dd3c7eb0b3248e2276d1b68c2
Ryota9 <ngm9dwf@gmail.com>
2022-03-04 16:53:00 +0900

    プロフィールの実装。

---
+++ app/models/profile.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :bigint           not null, primary key
+#  birthday   :date
+#  nickname   :string
+#  path       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+class Profile < ApplicationRecord
+    belongs_to :user
+    has_one_attached :avatar
+
+    def grade
+        return '不明' unless birthday.present?
+        # birthdayがなければ不明と返す
+        t = Time.now + 274.days
+        bd = birthday + 274.days
+        years = t.year - bd.year
+    
+        return "中学#{years - 12}年生"
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_034830) do
+ActiveRecord::Schema.define(version: 2022_03_04_041536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2022_03_03_034830) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "nickname"
+    t.string "path"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
