b4e35238f5fd633a58db4900ed12b9d5a2f72788
Ryota9 <ngm9dwf@gmail.com>
2022-03-17 19:18:39 +0900

    password酷かったので修正。

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: 'maehataryota', password: 'password', admin: true)
+User.create!(username: 'maehataryota', password: 'zemi1993', admin: true)
