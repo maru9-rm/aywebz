470956357dfbd93d13ea34df670ed4d9f2ab1411
Ryota9 <ngm9dwf@gmail.com>
2022-03-23 12:26:45 +0900

    adminユーザーなら他人のお知らせも編集可能に。見た目もちょっと修正。

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -13,21 +13,21 @@ class Admin::NotificationsController < Admin::ApplicationController
     def create
         @notification = current_user.notifications.build(notification_params)
         if @notification.save
-          redirect_to admin_notifications_path, notice: 'Saved in successfully.'
+          redirect_to admin_notifications_path, notice: 'お知らせを作成しました。'
         # notice: でフラッシュメッセージにテキスト(ハッシュ)を持たせてリクエストを送ることができる。
         else
-          flash.now[:error] = 'Failed to save. Please check the error message.'
+          flash.now[:error] = 'お知らせの作成に失敗しました。'
           render :new
           # この場合はレンダーなので、URLにリクエストを送るわけではないため、その場ですぐフラッシュを表示させるためにflash.nowで！
         end
     end
 
     def edit
-      @notification = current_user.notifications.find(params[:id])
+      @notification = Notification.find(params[:id])
     end
   
     def update
-      @notification = current_user.notifications.find(params[:id])
+      @notification = Notification.find(params[:id])
       if @notification.update(notification_params)
         redirect_to root_path, notice: '更新できました'
       else
@@ -39,7 +39,7 @@ class Admin::NotificationsController < Admin::ApplicationController
     
 
     def destroy
-      notification = current_user.notifications.find(params[:id])
+      notification = Notification.find(params[:id])
       notification.destroy!
       redirect_to root_path, notice: '削除に成功しました'
 
